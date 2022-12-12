5a47661552c05042d078b237eecf50ba1a96ff8d
Abhishek <abhishek.pandey@ucertify.com>
2022-12-12 15:06:35 +0530

    prac 7

---
+++ myapp/src/routes/eventhandling/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let count =0;
+    function handleClick(){
+        count=count+1;
+    }
+</script>
+
+<main>
+<button on:click={handleClick}>Count{count}</button>
+</main>
+
+<style>
+
+</style>
