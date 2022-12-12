a859dbc843860fa49a27dd714e7fb4157ad6ff5b
Abhishek <abhishek.pandey@ucertify.com>
2022-12-12 11:17:51 +0530

    prac 4

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -4,6 +4,8 @@ const last_name ="<b>pandey</b>";
 const hack = `<a href="#" onclick="alert('You have been hacked!')">Win a prize!</a>`;
 const id="heading";
 const disabled =true;
+const status = "success";
+const is_promoted =true;
 </script>
 
 
@@ -14,11 +16,26 @@ const disabled =true;
 <div>{@html hack}</div>
 <div {id}>This is id</div>
 <button {disabled}>This is button</button>
+<h2 class="underline">Underline text</h2>
+<h2 class={status}>Status</h2>
+<div class={is_promoted ? 'promoted': ''}>Movie Title</div>
 </main>
 
 
 
 <style>
+    .promoted{
+        font-style: italic;
+    }
+    .danger{
+        color:red;
+    }
+    .success{
+        color: olivedrab;
+    }
+    .underline{
+        text-decoration: underline;
+    }
     main{
         text-align: center;
         margin: 0 auto;
