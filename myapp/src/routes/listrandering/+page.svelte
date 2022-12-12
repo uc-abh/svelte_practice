21431b0366feb6d5227d5270d708d62a10a1b237
Abhishek <abhishek.pandey@ucertify.com>
2022-12-12 12:41:04 +0530

    prac 6

---
+++ myapp/src/routes/listrandering/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+
+const list =["Abhishek", "Pandey", "is", "a", "good", "boy"];
+
+const new_list=[
+    {first:'Abhishek', last:'Pandey'},
+    {first:'Ankit', last:'Pandey'},
+    {first:'Jai', last:'Bajarangbali'},
+    {first:'Jai', last:'shree Ram'},
+    
+]
+</script>
+
+<main>
+{#each list as value, index}
+<h2>{index+1}. {value}</h2>
+{/each}
+    
+
+
+{#each new_list as name}
+<h2>{name.first} {name.last}</h2>
+
+{/each}
+</main>
+
+
+<style>
+
+</style>
