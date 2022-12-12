f597d4a6ea2b83c9c282ea48c43fa0918f60f7a4
Abhishek <abhishek.pandey@ucertify.com>
2022-12-12 11:50:33 +0530

    prac 5

---
+++ myapp/src/routes/about/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Myapp from '../../Components/Myapp.svelte'
+    import Footer from '../../Components/Footer.svelte'
+    const num="5";
+</script>
+
+<main>
+{#if num===0}
+<h2>This is zero</h2>
+{:else if num <0}
+<h2>Number is negative number</h2>
+{:else if num>0}
+<h2>This is positive number</h2>
+{:else}
+<h2>This is not a nuumber</h2>
+{/if}
+</main>
+<Myapp/>
+
+<Footer/>
+
+<style>
+    
+</style>
