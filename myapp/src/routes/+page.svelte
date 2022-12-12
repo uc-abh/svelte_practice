46381ee161dfcecc7a72d49234899467453a7808
Abhishek <abhishek.pandey@ucertify.com>
2022-12-12 09:38:08 +0530

    prac 1

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+const first_name ="Abhishek";
+const last_name ="pandey";
+</script>
+
+
+<main>
+<h1>Hello world</h1>
+<div>Channel</div>
+<div>{first_name} {last_name}</div>
+</main>
+
+
+
+<style>
+    main{
+        text-align: center;
+        margin: 0 auto;
+    }
+    h1{
+        color: orangered;
+        text-transform: uppercase;
+        font-size:4rem;
+        font-weight: 100;
+    }
+
+</style>
